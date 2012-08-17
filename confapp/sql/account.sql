081d3c231dcf782d997f616cb650d528b5cf1a04
Robert Nowotniak <robert@nowotniak.com>
2012-08-17 04:05:25 +0200

    Drobne porządki

---
+++ confapp/sql/account.sql
@@ -0,0 +1,2 @@
+INSERT INTO "auth_user" VALUES(1,'rnowotniak@kis.p.lodz.pl','Robert','Nowotniak','rnowotniak@kis.p.lodz.pl','pbkdf2_sha256$10000$Nva6NJtkSJTq$4Y7rbUq42jBPTF8VTSaFDO/viwfjyYAL/n2mRonX0KY=',1,1,1,'2012-08-17 00:43:50.320173','2012-08-15 00:59:24');
+INSERT INTO "confapp_account" VALUES(1,1,'Politechnika Łódzka','KIS','Lodz','90-244','Stefanowskiego ....','5829538192',1,'509-719-046','');
